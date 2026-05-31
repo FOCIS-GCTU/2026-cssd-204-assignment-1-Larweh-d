2abf65daa7d3211c1479873e99da5d26d7997c41
Larweh-d <2425403458@live.gctu.edu.gh>
2026-05-31 19:33:26 +0000

    Assignment 2

--- initials.py
+++ initials.py
@@ -1,7 +1,29 @@
+# File: initials.py
+# Description: Print out my initials
+# Assignment Number: 1
+#
+# Name: OBED LARWEH
+# STUDENT ID: 2425403458
+# Email: 2425403458@live.gctu.edu.gh
+# Grader: Augustus
+#
+#On my honor, Obed Larweh, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
-
-
-
+    # Print my initials in small and large stylized letters.
+    print()
+    print("...OLL")
+    print()
+    print("...OOOOOOOOOOOO........LL.................LL..............")
+    print("...OO........OO........LL.................LL..............")
+    print("...OO........OO........LL.................LL..............")
+    print("...OO........00........LL.................LL..............")
+    print("...OO........OO........LL.................LL..............")
+    print("...OO........OO........LL.................LL..............")
+    print("...OO........OO........LL.................LL..............")
+    print("...OO........OO........LL.................LL..............")
+    print("...OO........00...**...LL..............**.LL..............")
+    print("...O00000000000...**...LLLLLLLLLLLL....**.LLLLLLLLLLLL....")
+    print()
 main()
